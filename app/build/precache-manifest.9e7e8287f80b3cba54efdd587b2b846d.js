e1eaa35219129651e586ec77313a4e8e72eb02d9
Maxime JOUBERT <m.joubert.92@gmail.com>
2021-11-08 19:58:44 +0100

    build

---
+++ app/build/precache-manifest.9e7e8287f80b3cba54efdd587b2b846d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77edfb1ae051758705446836328d65c6",
+    "url": "/dapp-twitter/index.html"
+  },
+  {
+    "revision": "de004fa17d5c2490ee63",
+    "url": "/dapp-twitter/static/css/main.72030115.chunk.css"
+  },
+  {
+    "revision": "052c0be8e456b5620215",
+    "url": "/dapp-twitter/static/js/2.20b04905.chunk.js"
+  },
+  {
+    "revision": "de004fa17d5c2490ee63",
+    "url": "/dapp-twitter/static/js/main.25c06fc5.chunk.js"
+  },
+  {
+    "revision": "e6eb20e6acc636598edd",
+    "url": "/dapp-twitter/static/js/runtime-main.6521a9fe.js"
+  }
+]);
